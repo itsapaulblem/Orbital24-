b5788eff39892ba21d569fd7277df86551477699
Ng Kay Hian <e1113766@u.nus.edu>
2024-05-18 12:59:31 +0800

    Relink Start btn to BaseLevel

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,6 +7,6 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync("LevelOne");
+        SceneManager.LoadSceneAsync("BaseLevel");
     }
 }
