8e7e4198d13bf6149d179ac584833f9159e1ff72
Ng Kay Hian <e1113766@u.nus.edu>
2024-05-19 22:50:26 +0800

    Added animation to player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,10 +9,14 @@ public class PlayerController : MonoBehaviour
     private PlayerControls playerControls;
     private Vector2 movement;
     private Rigidbody2D rb;
+    private Animator playerAnimator;
+    private SpriteRenderer playerSpriteRenderer;
 
     private void Awake() {
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+        playerSpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnEnable() {
@@ -28,14 +32,24 @@ public class PlayerController : MonoBehaviour
     }
 
     private void FixedUpdate() {
+        UpdatePlayerFacingDirection();
         Move();
     }
 
     private void PlayerInput() {
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
+        movement.Normalize();
+        playerAnimator.SetFloat("moveX", movement.x);
+        playerAnimator.SetFloat("moveY", movement.y);
     }
 
     private void Move() {
         rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+
+    private void UpdatePlayerFacingDirection() {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 cameraPoint = Camera.main.WorldToScreenPoint(transform.position);
+        playerSpriteRenderer.flipX = mousePos.x < cameraPoint.x;
     }
 }
