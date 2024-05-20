61af212ce5f797c3a736be39a8e0ae5748000964
Paul Cheng <paulcheng46@gmail.com>
2024-05-20 18:27:37 +0800

    added music and updated interface

---
+++ Assets/Scripts/VolumeSettings.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeSettings : MonoBehaviour
+{
+    [SerializeField] private AudioMixer myAudio;
+    [SerializeField] private Slider musicSlider;
+    // Start is called before the first frame update
+    private void Start(){
+        if (PlayerPrefs.HasKey("musicVolume")){
+            LoadVolume();
+        }
+        else{
+            SetMusicVolume();
+        }
+       // SetMusicVolume();
+    }
+    public void SetMusicVolume(){
+        float volume = musicSlider.value;
+        myAudio.SetFloat("music", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+    }
+
+    private void LoadVolume(){
+        musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        SetMusicVolume();
+    }
+}
