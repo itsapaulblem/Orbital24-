d459ffc46d044b0900c8c1f9c46dad75fa351c6c
Paul Cheng <paulcheng46@gmail.com>
2024-06-07 14:30:44 +0800

    updated dialogue system

--- Assets/Scripts/Town/ActivateTextAtLine.cs
+++ Assets/Scripts/Town/ActivateTextAtLine.cs
@@ -1,49 +1,63 @@
-using UnityEngine;
-
-public class ActivateTextAtLine : MonoBehaviour
-{
-    public TextAsset theText;
-    public int startLine;
-    public int endLine;
-    public TextBoxManager theTextBox;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        theTextBox = FindObjectOfType<TextBoxManager>();
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            Debug.Log("fdfb");
-            theTextBox.showPrompt(true); // Show prompt when player is near
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            theTextBox.showPrompt(false); // Hide prompt when player moves away
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (theTextBox.isPlayerNear && Input.GetKeyDown(KeyCode.E))
-        {
-            ActivateTextBox(); // Activate text box when player presses 'E' while being near
-        }
-    }
-
-    void ActivateTextBox()
-    {
-        theTextBox.ReloadScript(theText);
-        theTextBox.currentLine = startLine;
-        theTextBox.endAtLine = endLine;
-        theTextBox.EnableTextBox();
-    }
+using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine; 
+ 
+public class ActivateTextAtLine : MonoBehaviour 
+{ 
+    public TextAsset theText; 
+    public int startLine; 
+    public int endLine; 
+    public TextBoxManager theTextBox; 
+    public bool requireButtonPress; 
+    private bool waitForPress; 
+    public bool destroyWhenActivated; 
+ 
+    // Start is called before the first frame update 
+    void Start() 
+    { 
+        theTextBox = FindObjectOfType<TextBoxManager>(); 
+    } 
+ 
+    // Update is called once per frame 
+    void Update() 
+    { 
+        if (waitForPress && Input.GetKeyDown(KeyCode.E)) 
+        { 
+            ActivateTextBox(); 
+        } 
+    } 
+ 
+    void OnTriggerEnter2D(Collider2D other) 
+    { 
+        if (other.CompareTag("Player")) 
+        { 
+            if (requireButtonPress) 
+            { 
+                waitForPress = true; 
+                return; 
+            } 
+            ActivateTextBox(); 
+        } 
+    } 
+ 
+    void OnTriggerExit2D(Collider2D other) 
+    { 
+        if (other.CompareTag("Player")) 
+        { 
+            waitForPress = false; 
+        } 
+    } 
+ 
+    void ActivateTextBox() 
+    { 
+        theTextBox.ReloadScript(theText); 
+        theTextBox.currentLine = startLine; 
+        theTextBox.endAtLine = endLine; 
+        theTextBox.EnableTextBox(); 
+ 
+        if (destroyWhenActivated) 
+        { 
+            Destroy(gameObject); 
+        } 
+    } 
 }
--- Assets/Scripts/Town/TextBoxManager.cs
+++ Assets/Scripts/Town/TextBoxManager.cs
@@ -1,120 +1,135 @@
-using System.Collections;
-using UnityEngine;
-using UnityEngine.UI;
-
-public class TextBoxManager : MonoBehaviour
-{
-    public GameObject prompt;
-    public GameObject speechPanel;
-    public Text theText;
-    public TextAsset textFile;
-    public string[] textLines;
-    public int currentLine;
-    public int endAtLine;
-    public PlayerController player;
-
-    public bool isTextboxActive; 
-    private bool isTyping = false;
-    private bool cancelTyping = false;
-    public float typeSpeed;
-    public bool isPlayerNear = false; 
-
-    void Start()
-    {
-        player = FindObjectOfType<PlayerController>();
-
-        if (textFile != null)
-        {
-            textLines = textFile.text.Split(new[] { "\r\n", "\r", "\n" }, System.StringSplitOptions.None);
-        }
-
-        if (endAtLine == 0 && textLines != null)
-        {
-            endAtLine = textLines.Length - 1;
-        }
-    }
-
-    void Update()
-    {
-        // Show prompt if textbox is not active and player is near
-        if (!isTextboxActive && isPlayerNear)
-        {
-            prompt.SetActive(true);
-            if (Input.GetKeyDown(KeyCode.E)){
-                prompt.SetActive(false);
-                EnableTextBox();
-            }
-        }
-
-        // Check for 'E' key press when player is near
-        if (isTextboxActive && Input.GetKeyDown(KeyCode.E))
-        {
-            if (!isTyping)
-            {
-                currentLine += 1;
-                if (currentLine > endAtLine)
-                {
-                    DisableTextBox();
-                }
-                else
-                {
-                    StartCoroutine(TextScroll(textLines[currentLine]));
-                }
-            }
-            else if (isTyping && !cancelTyping)
-            {
-                cancelTyping = true;
-            }
-        }
-    }
-
-    private IEnumerator TextScroll(string lineOfText)
-    {
-        int letter = 0;
-        theText.text = "";
-        isTyping = true;
-        cancelTyping = false;
-        while (isTyping && !cancelTyping && (letter < lineOfText.Length))
-        {
-            theText.text += lineOfText[letter];
-            letter += 1;
-            yield return new WaitForSeconds(typeSpeed);
-        }
-        theText.text = lineOfText;
-        isTyping = false;
-        cancelTyping = false;
-    }
-
-    public void EnableTextBox()
-    {
-        if (textLines == null || textLines.Length == 0)
-        {
-            return;
-        }
-        speechPanel.SetActive(true);
-        prompt.SetActive(false);
-        isTextboxActive = true; // Update flag to indicate textbox is active
-        StartCoroutine(TextScroll(textLines[currentLine]));
-    }
-
-    public void DisableTextBox()
-    {
-        speechPanel.SetActive(false);
-        isTextboxActive = false; // Update flag to indicate textbox is inactive
-    }
-
-    public void ReloadScript(TextAsset newText)
-    {
-        if (newText != null)
-        {
-            textLines = newText.text.Split(new[] { "\r\n", "\r", "\n" }, System.StringSplitOptions.None);
-            currentLine = 0;
-            endAtLine = textLines.Length - 1;
-        }
-    }
-
-    public void showPrompt(bool show)
-    {
-        prompt.SetActive(show);
-    }
+using System.Collections; 
+using System.Runtime.CompilerServices; 
+using UnityEngine; 
+using UnityEngine.UI; 
+ 
+public class TextBoxManager : MonoBehaviour 
+{ 
+    public GameObject textBox; 
+    public Text theText; 
+    public TextAsset textFile; 
+    public string[] textLines; 
+    public int currentLine; 
+    public int endAtLine; 
+    public PlayerController player; 
+ 
+    public bool isActive; 
+    public bool stopPlayerMovement; 
+ 
+    private bool isTyping = false;  
+    private bool cancelTyping = false;  
+    public float typeSpeed; 
+ 
+    // Start is called before the first frame update 
+    void Start() 
+    { 
+        player = FindObjectOfType<PlayerController>(); 
+ 
+        if (textFile != null) 
+        { 
+            textLines = textFile.text.Split(new[] { "\r\n", "\r", "\n" }, System.StringSplitOptions.None); 
+        } 
+ 
+        if (endAtLine == 0 && textLines != null) 
+        { 
+            endAtLine = textLines.Length - 1; 
+        } 
+ 
+        if (isActive) 
+        { 
+            EnableTextBox(); 
+        } 
+        else 
+        { 
+            DisableTextBox(); 
+        } 
+    } 
+ 
+    void Update() 
+    { 
+        if (!isActive) 
+        { 
+            return; 
+        } 
+ 
+        if (currentLine <= endAtLine && textLines != null && currentLine < textLines.Length) 
+        { 
+            // theText.text = textLines[currentLine]; 
+        } 
+ 
+        if (Input.GetKeyDown(KeyCode.E)) 
+        { 
+            if (!isTyping) 
+            { 
+                currentLine += 1; 
+                if (currentLine > endAtLine) 
+                { 
+                    DisableTextBox(); 
+                } 
+                else 
+                { 
+                    StartCoroutine(TextScroll(textLines[currentLine])); 
+                } 
+            } 
+            else if (isTyping && !cancelTyping) 
+            { 
+                cancelTyping = true; 
+            } 
+        } 
+    } 
+ 
+    private IEnumerator TextScroll(string lineOfText) 
+    { 
+        int letter = 0; 
+        theText.text = ""; 
+        isTyping = true; 
+        cancelTyping = false; 
+        while (isTyping && !cancelTyping && (letter < lineOfText.Length)) 
+        { 
+            theText.text += lineOfText[letter]; 
+            letter += 1; 
+            yield return new WaitForSeconds(typeSpeed); 
+        } 
+        theText.text = lineOfText; 
+        isTyping = false; 
+        cancelTyping = false; 
+    } 
+ 
+    public void EnableTextBox() 
+    { 
+        if (textLines == null || textLines.Length == 0) 
+        { 
+            return; 
+        } 
+        textBox.SetActive(true); 
+        isActive = true; 
+ 
+        if (player != null) 
+        { 
+         //   player.canMove = false; 
+        } 
+ 
+        StartCoroutine(TextScroll(textLines[currentLine])); 
+    } 
+ 
+    public void DisableTextBox() 
+    { 
+        textBox.SetActive(false); 
+        isActive = false; 
+ 
+        if (player != null) 
+        { 
+       //     player.canMove = true; 
+        } 
+    } 
+ 
+    public void ReloadScript(TextAsset newText) 
+    { 
+        if (newText != null) 
+        { 
+            textLines = newText.text.Split(new[] { "\r\n", "\r", "\n" }, System.StringSplitOptions.None); 
+            currentLine = 0; 
+            endAtLine = textLines.Length - 1; 
+        } 
+    } 
 }
