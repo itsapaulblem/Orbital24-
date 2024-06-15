5e0490797a24dc80cfb10ec71041fb63402eba39
Ng Kay Hian <e1113766@u.nus.edu>
2024-06-15 14:04:46 +0800

    added auto-despawn to enemy through spawner
    EnemySpawner will handle despawn behaviour based on distance from
    player.

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -9,22 +9,37 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private int enemyCap = 3;
     private GameObject player;
     private List<GameObject> enemies = new List<GameObject>();
-    private float temptime = 10f;
-    private float templast;
+    private bool canSpawn = false;
+    private float spawnSight = 35f; // spawn if player within
     
 
     private void Start()
     {
-        StartCoroutine(Spawner());
         player = GameObject.Find("Player");
-        templast = Time.time - temptime;
+    }
+
+    private void Update() 
+    {
+        if (Vector2.Distance(player.transform.position, transform.position) <= spawnSight && !canSpawn) {
+            canSpawn = true;
+            StartCoroutine(Spawner());
+        } else if (Vector2.Distance(player.transform.position, transform.position) > spawnSight) {
+            canSpawn = false;
+            foreach (GameObject e in enemies) {
+                if (e != null && Vector2.Distance(player.transform.position, e.transform.position) > spawnSight) {
+                    Destroy(e);
+                }
+            }
+            enemies.RemoveAll(e => e == null);
+        }
+            
     }
 
     private IEnumerator Spawner()
     {
         WaitForSeconds wait = new WaitForSeconds(spawnRate);
 
-        while (true) {
+        while (canSpawn) {
             if (enemies.Count >= enemyCap) {
                 enemies.RemoveAll(e => e == null);
             }
@@ -32,7 +47,7 @@ public class EnemySpawner : MonoBehaviour
                 int rand = UnityEngine.Random.Range(0, enemyPrefabs.Length); // Changed here
                 GameObject enemyToSpawn = enemyPrefabs[rand];
 
-                Vector2 spawnLocation = transform.position + new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0).normalized * 2;
+                Vector2 spawnLocation = transform.position + new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0).normalized * 5;
 
                 GameObject spawnedEnemy = Instantiate(enemyToSpawn, spawnLocation, Quaternion.identity);
 
