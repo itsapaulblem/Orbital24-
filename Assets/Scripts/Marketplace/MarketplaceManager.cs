c9cf4d6adf4e7cd595b1946bfdcfa9d6a8f2a7fd
Paul Cheng <paulcheng46@gmail.com>
2024-07-09 21:06:49 +0800

    added comments on marketplace manager

--- Assets/Scripts/Marketplace/MarketplaceManager.cs
+++ Assets/Scripts/Marketplace/MarketplaceManager.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+/**
+ * MarketplaceManager is a script that manages the marketplace UI and logic.
+ * It is responsible for displaying shop items, handling purchases, and updating the coin UI.
+ */
 public class MarketplaceManager : MonoBehaviour
 {
     private AudioManager audioManager;
+    /**
+     * An array of shop items to be displayed in the marketplace.
+     */
     public ShopItem[] shopItems; 
+    /**
+     * An array of game objects that represent the shop panels.
+     */
     public GameObject[] shopPanelsGameObject; 
+    /**
+     * An array of shop templates that contain UI elements for each shop item.
+     */
     public ShopTemplate[] shopPanels; 
+    /**
+     * An array of purchase buttons that allow the player to buy items.
+     */
     public Button[] purchaseButton;
+    /**
+     * A UI element that displays the player's current coin count.
+     */
     public TMP_Text coins_UI;  // UI element to display coins in the marketplace
 
+    /**
+     * Initializes the marketplace manager.
+     */
     void Start()
     {
         audioManager = AudioManager.Instance;
@@ -26,6 +48,9 @@ public class MarketplaceManager : MonoBehaviour
         CheckPurchaseable();
     }
 
+    /**
+     * Loads the shop panels with item data.
+     */
     public void LoadPanels()
     {
         for (int i = 0; i < shopItems.Length; i++)
@@ -36,6 +61,9 @@ public class MarketplaceManager : MonoBehaviour
         }
     }
 
+    /**
+     * Checks which items are purchaseable based on the player's current coin count.
+     */
     public void CheckPurchaseable()
     {
         int coins = Inventory.GetCoins();
@@ -52,6 +80,12 @@ public class MarketplaceManager : MonoBehaviour
         }
     }
 
+    /**
+     * Purchases an item from the shop.
+     * 
+     * @param num The index of the item to purchase.
+     * @example PurchaseItem(0) would purchase the first item in the shop.
+     */
     public void PurchaseItem(int num)
     {
         if (Inventory.GetCoins() >= shopItems[num].baseCost)
